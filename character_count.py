3ca4f60a1b011078e137d7736c38367100d1b1a9
pacocoursey <pvco.coursey@gmail.com>
2019-01-18 13:11:45 -0700

    read in filename from command line, remove fstats file

--- character_count.py
+++ character_count.py
@@ -1,10 +1,27 @@
-f_name = input("What is the name of the file?\n")
+import sys
 
-file_in = open(f_name, 'r')
-num_chars = 0
+def usage():
+    print("\n  Usage:")
+    print("    python character_count.py [filename]\n")
 
-for line in file_in:
-    word_list = line.split()
-    num_chars += sum(len(word) for word in word_list) #produces length of each word and appends to sum
+# Ensure filename is passed as command line argument
+if len(sys.argv) < 2:
+    print("Filename not specified.")
+    usage()
+    sys.exit(-1)
 
-print(str(num_chars) + " Characters")
+f_name = sys.argv[1]
+
+try:
+    file_in = open(f_name, 'r')
+    num_chars = 0
+
+    for line in file_in:
+        word_list = line.split()
+
+        # Produces length of each word and appends to sum
+        num_chars += sum(len(word) for word in word_list)
+
+        print(str(num_chars) + " Characters")
+except FileNotFoundError:
+    print(f"The file {f_name} cannot be opened.")
