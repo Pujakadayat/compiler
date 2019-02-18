1afc614c0d374b5924366a5d54d7908c468577d1
Charles <charlesherrmann8@gmail.com>
2019-02-18 16:30:19 -0700

    Added comments for lrTable

--- src/parser/lrTable.py
+++ src/parser/lrTable.py
@@ -36,20 +36,24 @@ class LRTable:
 
     def buildTable(self):
         # Augment rules with accepting state
-        self.rules["ACC"] = [["program"]]
+        self.rules["ACC"] = [["S"]]
 
         # Parse the input grammar
         self.parseGrammar()
 
-        # Add the accepting state to grammar
-        self.itemSets[0] = [item("ACC", "program", 0, "$")]
+        # Start itemset 0 with the accepting state
+        self.itemSets[0] = [item("ACC", "S", 0, "$")]
 
         # close Itemsets and create new sets until no more
         i = 0
         while self.hasItemSet(i):
+            # close out itemset i
             self.closure(i)
+            # change nonterminal followers to terminals in itemset i
             self.cleanItemSet(i)
+            # create any new itemsets from itemset i
             self.createItemSets(i)
+            # search through itemsets and remove any copies
             self.cleanItemSets()
             i = i + 1
 
@@ -61,6 +65,11 @@ class LRTable:
         self.printTransitions()
         self.printTable()
 
+    # parse the input grammar into rules
+    #   the variable self.rules is filled here
+    #   self.rules in a dictionary with the lhs of a rule as the key
+    #   and lists as the value. The lists are the different rhs's that
+    #   the rule points to.
     def parseGrammar(self):
         # Open file with grammar
         lines = self.grammar.splitlines()
@@ -68,27 +77,32 @@ class LRTable:
         # parse grammar file into rules
         for line in lines:
             rule = line.split(" ")
-
+            # Check to see if valid format
             if rule[1] == "->":
-                # seperate | in the rule
+                # seperate the "|" out of the rule
                 last = 2
                 for i in range(len(rule)):
                     if rule[i] == "|":
+                        # if lhs of rule is in self.rules, append rhs of rule to the list
                         if rule[0] in self.rules.keys():
                             self.rules[rule[0]].append(rule[last:i])
+                        # if lhs of rule is not in self.rules, add new dictionary element
                         else:
                             self.rules[rule[0]] = [rule[last:i]]
                         last = i + 1
-
+                # for loop above catches all but the last rule from the grammar
+                # this if-else is to catch that final rule
                 if rule[0] in self.rules.keys():
                     self.rules[rule[0]].append(rule[last:])
                 else:
                     self.rules[rule[0]] = [rule[last:]]
 
+        # add all the nonTerminals to self.nonTerminal list
         for k in self.rules.keys():
             if k not in self.nonTerminals:
                 self.nonTerminals.append(k)
 
+        # add all the terminals to the self.terminals list
         for v in self.rules.values():
             for tokenList in v:
                 for token in tokenList:
@@ -96,81 +110,108 @@ class LRTable:
                         self.terminals.append(token)
 
     # close out an itemset
+    # this involves expanding out rules from the grammar
     def closure(self, setNum):
+        # newSet is just the itemset we are currently interested in
         newSet = self.itemSets[setNum]
         done = False
+        # keep looping until no more rules can be expanded
         while not done:
-            for i in newSet:
-                # print(i)
+            # for each item in the set:
+            for currItem in newSet:
                 new = False
-                a = i.getRightAfter()
-                b = i.getAfter()
-                following = i.following
+                # get the token after the dot
+                a = currItem.getRightAfter()
+                # get all the tokens after the token after the dot
+                b = currItem.getAfter()
+                # following becomes the first token after the token after the seperator
+                # if there is a token after the token after the seperator
+                # if not, following is just the o
+                following = currItem.following
                 if len(b) > 0:
                     following = b[0]
+                # if a is a nonTerminal, expand rule
                 if a in self.rules.keys():
                     new = True
+                    # for each rule for nonTerminal a:
                     for rule in self.rules[a]:
+                        # make a new item for nonTerminal a rule
                         rhs = " ".join(rule)
                         newItem = item(a, rhs, 0, following)
+                        # check if item already exists in the itemSet
                         isNew = True
                         for tempItem in newSet:
                             if newItem.isSame(tempItem):
                                 isNew = False
+                        # if item is new then add it to set
                         if isNew:
                             newSet.append(newItem)
                             new = True
                             # print("\t", newItem)
+                # if no new items found we are done
                 if not new:
                     done = True
-        return newSet
 
-    # remove any nonterm following from item sets
+    # remove any nonterm following token from itemSet
     def cleanItemSet(self, setNum):
         nonTerms = True
+        # loop until no more nonTerminal following tokens found in itemset
         while nonTerms:
-            # self.printItemSets()
             nonTerms = False
             for itemNum in range(len(self.itemSets[setNum])):
+                # if item.following in itemSet is a nonTerm then expand the nonTerm into terminal
                 if self.itemSets[setNum][itemNum].following in self.nonTerminals:
-                    # print(setNum, itemNum)
                     nonTerms = True
                     lhs = self.itemSets[setNum][itemNum].lhs
                     rhs = self.itemSets[setNum][itemNum].rhs
                     sep = self.itemSets[setNum][itemNum].seperator
                     following = self.itemSets[setNum][itemNum].following
+                    # for each rule the nonTerminal token expands into:
                     for numRule in range(len(self.rules[following])):
-                        # print(self.rules[following], numRule)
                         newItem = item(lhs, rhs, sep, self.rules[following][numRule][0])
+                        # check to see if new item is new to the set
                         isNew = True
                         for tempItem in self.itemSets[setNum]:
                             if newItem.isSame(tempItem):
                                 isNew = False
+                        # if it is, add it to the itemSet
                         if isNew:
                             self.itemSets[setNum].append(newItem)
-                            # print("Added item")
-                            # self.printItemSets()
+                    # delete the old rule with the nonTerminal following token
                     del self.itemSets[setNum][itemNum]
                     break
 
+    # create new itemSets from past itemSets
+    # this is tracked with the transition table
     def createItemSets(self, setNum):
+        # for each item in itemSet[setNum]
         for currItem in self.itemSets[setNum]:
+            # make a newItem from the currItem with the seperator incremented by 1
             newItem = currItem.incSeperator()
+            # get the delimeter for the transition table
             delimeter = newItem.getRightBefore()
+            # if delimeter exists
             if len(delimeter) > 0:
+                # if the transitions for itemSet[setNum] does not exist add it
                 if setNum not in self.transitions.keys():
                     self.transitions[setNum] = {}
+                # if delimeter does not exist in transition[setNum] add it and increment self.setNum by 1
                 if delimeter not in self.transitions[setNum].keys():
                     self.setNum = self.setNum + 1
                     self.transitions[setNum][delimeter] = self.setNum
-                # self.printTransitions()
+                # if the new itemSet does not exist in self.itemSets add it
                 if self.transitions[setNum][delimeter] not in self.itemSets.keys():
                     self.itemSets[self.transitions[setNum][delimeter]] = []
+                # add the newItem to the new set
                 self.itemSets[self.transitions[setNum][delimeter]].append(newItem)
 
+    # clean itemSets of any identical sets
     def cleanItemSets(self):
+        # compare the itemSets backwards
+        # So compare every set with the sets that came before it
         for i in range(self.setNum, -1, -1):
             for j in range(i - 1, -1, -1):
+                # compare set i with set j
                 same = True
                 for itemSet in self.itemSets[j]:
                     inThere = False
@@ -180,44 +221,42 @@ class LRTable:
                     if not inThere:
                         same = False
                         break
+                # if itemSets i and j are identical delete itemSet i (the itemSet that came later)
                 if same:
-                    # print(i, " is the same as ", j)
                     del self.itemSets[i]
-                    # self.printItems()
+                    # self.setNum is now the lowest available set number
                     self.updateSetNum()
-                    # print("u ", self.setNum)
+                    # update the transition table so any reference of itemSet i becomes itemSet j
                     for k1, v1 in self.transitions.items():
                         for k2, v2 in v1.items():
                             if self.transitions[k1][k2] == i:
                                 self.transitions[k1][k2] = j
-                                # print(k1, k2)
                     break
-
+    
+    # build the action and goto tables from the itemSets and the transition table
     def buildActionGoto(self):
+        # go through itemSets to get reduction rules
         for itemSetNum, itemSet in self.itemSets.items():
             for item in itemSet:
                 if self.seperatorAtEnd(item):
-                    # print("[", itemSetNum, item, "]")
                     for k, v in self.rules.items():
                         if item.lhs == k:
-                            # print("found rules", k, v, "for item")
                             for i in range(len(v)):
-                                # print("maybe: ", item.rhs, " == ", v[i], "?")
                                 if item.rhs == " ".join(v[i]):
-                                    # print("This rule: ", self.rules[k][i])
                                     if itemSetNum not in self.actions.keys():
                                         self.actions[itemSetNum] = {}
                                     self.actions[itemSetNum][
                                         item.following
                                     ] = "r %s %i" % (k, i)
+        # go through transition table to get:
         for k1, v1 in self.transitions.items():
-            # print(k1)
             for k2, v2 in v1.items():
-                # print(k2, v2)
+                # goto rules
                 if k2 in self.nonTerminals:
                     if k1 not in self.goto.keys():
                         self.goto[k1] = {}
                     self.goto[k1][k2] = v2
+                # shift rules
                 else:
                     if k1 not in self.actions.keys():
                         self.actions[k1] = {}
@@ -336,6 +375,11 @@ class LRTable:
             print(k, v)
 
 
+# an item is like a grammar rule
+# there is a left hand side of the rule (lhs)
+# and a right hand side (rhs)
+# a seperator that is the dot thing
+# and a following token for the rule
 class item:
     def __init__(self, lhs, rhs, seperator, following):
         self.lhs = lhs
