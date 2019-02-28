6fe596012aabfd497263d3ee8b486b0acff0d9be
Rubikk <iseaholguin@gmail.com>
2019-02-28 15:46:49 -0700

    better directory integration

---
+++ tests/testing.py
@@ -0,0 +1,183 @@
+"""
+Each test case has an accompanying class.
+Each have methods such as: test_lexer, test_parser & test_symbolTable
+"""
+
+"""
+Usage: python3 -m testing.py
+"""
+
+import unittest
+import main
+
+class AssignmentTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        self.assertEqual("✨ Completed scanning!".run(), "✨ Completed scanning!")
+
+    def test_islexer(self):
+        self.assertTrue("✨ Completed scanning!".run())
+        self.assertFalse("".run())
+"""
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class BasicMathTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class ExpressionTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class FloatTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class ForTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class FunctionTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class HelloWorldTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class IfElseTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class IfTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class IncludeTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class LineBreakTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class MathTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class MultiLineCommentTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class PlainTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+
+class SingleLineCommentTestCase(unittest.TestCase):
+
+    def test_lexer(self):
+        #self.
+
+    def test_parser(self):
+        #self.
+
+    def test_symbolTable(self):
+        #self.
+"""
+if __name__ == '__main__':
+    unittest.main()
