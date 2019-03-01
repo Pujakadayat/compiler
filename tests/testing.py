143f01ada0b8fa8687b1745390fa0fcef4c8f728
Rubikk <iseaholguin@gmail.com>
2019-02-28 17:14:25 -0700

    test for lexer & parser complete. Unsure how to implement for symbol table. Update: Currently passing 18 tests & failing 12; 30 total tests

--- tests/testing.py
+++ tests/testing.py
@@ -32,160 +32,300 @@ class AssignmentTestCase(unittest.TestCase):
 #        filename = "samples/assignment.c"
 #        grammar = "grammars/main_grammar.txt"
 #        flags = ["-"]
-"""
+
 class BasicMathTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/basic_math.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/basic_math.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class ExpressionTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/expression.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/expression.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class FloatTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/float.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/float.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class ForTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/for.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/for.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class FunctionTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/function.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/function.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class HelloWorldTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/hello_world.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/hello_world.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class IfElseTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/if_else.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/if_else.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class IfTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/if.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/if.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class IncludeTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/include.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/include.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class LineBreakTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/linebreak.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/linebreak.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class MathTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/math.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/math.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class MultiLineCommentTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/multi_line_comment.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/multi_line_comment.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class PlainTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/plain.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
-        #self.
+        filename = "samples/plain.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
 
-    def test_symbolTable(self):
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
 class SingleLineCommentTestCase(unittest.TestCase):
 
     def test_lexer(self):
-        #self.
+        filename = "samples/single_line_comment.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-s"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
 
     def test_parser(self):
+        filename = "samples/single_line_comment.c"
+        grammar = "grammars/main_grammar.txt"
+        flags = ["-p"]
+
+        isAcceptedByParser = main.run(filename, grammar, flags)
+        self.assertEqual(isAcceptedByParser, True)
+
+    #def test_symbolTable(self):
         #self.
 
-    def test_symbolTable(self):
-        #self.
-"""
 if __name__ == '__main__':
     unittest.main()
