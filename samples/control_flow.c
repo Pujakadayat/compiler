1f48fc1857927a92be838e6be10e1d292f689ca1
Charles <charlesherrmann8@gmail.com>
2019-03-29 14:03:46 -0600

    Added whileStatement to grammar. Made new control_flow.c sample file, causes bug in symbol table.

---
+++ samples/control_flow.c
@@ -0,0 +1,16 @@
+int main() {
+	int i = 0;
+    int y = 0;
+    int x = 0;
+
+	for (i = 0; i < 10; i++) {
+		x += 1;
+	}
+
+    while (i > 0) {
+        y += 2;
+        i--;
+    }
+
+	return 0;
+}
