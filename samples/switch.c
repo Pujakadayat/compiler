b393c27fcdd86a730930bafb0df8ec102454656f
pacocoursey <pvco.coursey@gmail.com>
2019-04-29 14:48:48 -0600

    Fix grammar for switch statements, update switch sample file

--- samples/switch.c
+++ samples/switch.c
@@ -1,16 +1,19 @@
 int main() {
-	int i = 10;
+	int i = 5;
 
 	switch(i) {
-		case 1:
+		case 1: {
 			i = 2;
 			break;
-		case 5:
+		}
+		case 5: {
 			i = 22;
 			break;
-		case 10:
+		}
+		case 10: {
 			i = 11;
 			break;
+		}
 	}
 
 	return i;
